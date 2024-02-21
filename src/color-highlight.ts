9972da06b7052b67078f12a9ddea1ac9f43f96a2
Mikhail Brinchuk <thecentury@gmail.com>
2024-02-21 21:55:42 +0100

    Parse and highlight .net GUIDs

--- src/color-highlight.ts
+++ src/color-highlight.ts
@@ -1,58 +1,52 @@
-import {
-    workspace,
-    window,
-    Range,
-    TextDocument,
-    Disposable
-  } from 'vscode';
+import { workspace, window, Range, TextDocument, Disposable } from "vscode";
 
-import { DecorationMap, DecorationOptions } from './decoration-map';
-import { HighlightedRange } from './strategies/strategy-types';
-import { dotnetGuids } from './strategies/dotnet-guid';
+import { DecorationMap, DecorationOptions } from "./decoration-map";
+import { HighlightedRange } from "./strategies/strategy-types";
+import { dotnetGuids } from "./strategies/dotnet-guid";
 
-  export class DocumentHighlight {
-    disposed : boolean;
-    document : TextDocument;
-    strategies : ((text : string) => Promise<HighlightedRange[]>)[];
-    decorations : DecorationMap;
-    listener : Disposable;
+export class DocumentHighlight {
+  disposed: boolean;
+  document: TextDocument;
+  strategies: ((text: string) => Promise<HighlightedRange[]>)[];
+  decorations: DecorationMap;
+  listener: Disposable;
 
-    constructor(document : TextDocument, viewConfig : DecorationOptions) {
-      this.disposed = false;
-  
-      this.document = document;
-      // todo initialize strategies
-      this.strategies = [dotnetGuids];
+  constructor(document: TextDocument, viewConfig: DecorationOptions) {
+    this.disposed = false;
+
+    this.document = document;
+    // todo initialize strategies
+    this.strategies = [dotnetGuids];
     //   this.strategies = [findFn, findHwb];
-  
+
     //   if (viewConfig.useARGB == true) {
     //     this.strategies.push(findHexARGB);
     //   } else {
     //     this.strategies.push(findHexRGBA);
     //   }
-  
+
     //   if (colorWordsLanguages.indexOf(document.languageId) > -1 || viewConfig.matchWords) {
     //     this.strategies.push(findWords);
     //   }
-  
+
     //   if (viewConfig.matchRgbWithNoFunction) {
     //     let isValid = false;
-  
+
     //     if (viewConfig.rgbWithNoFunctionLanguages.indexOf('*') > -1) {
     //       isValid = true;
     //     }
-  
+
     //     if (viewConfig.rgbWithNoFunctionLanguages.indexOf(document.languageId) > -1) {
     //       isValid = true;
     //     }
-  
+
     //     if (viewConfig.rgbWithNoFunctionLanguages.indexOf(`!${document.languageId}`) > -1) {
     //       isValid = false;
     //     }
-  
+
     //     if (isValid) this.strategies.push(findRgbNoFn);
     //   }
-  
+
     //   switch (document.languageId) {
     //     case 'css':
     //       this.strategies.push(findCssVars);
@@ -73,103 +67,116 @@ import { dotnetGuids } from './strategies/dotnet-guid';
     //       }));
     //       break;
     //   }
-  
+
     this.decorations = new DecorationMap(viewConfig);
-    this.listener = workspace.onDidChangeTextDocument(({ document }) => this.onUpdate(document));
+    this.listener = workspace.onDidChangeTextDocument(({ document }) =>
+      this.onUpdate(document)
+    );
+  }
+
+  onUpdate(document: TextDocument = this.document) {
+    if (
+      this.disposed ||
+      this.document.uri.toString() !== document.uri.toString()
+    ) {
+      return;
     }
-  
-    onUpdate(document : TextDocument = this.document) {
-      if (this.disposed || this.document.uri.toString() !== document.uri.toString()) {
+
+    const text = this.document.getText();
+    const version = this.document.version.toString();
+
+    console.log(`DocumentHighlight.onUpdate: ${document.fileName} v${version}`);
+
+    return this.updateRange(text, version);
+  }
+
+  /**
+   * @param {string} text
+   * @param {string} version
+   *
+   * @memberOf DocumentHighlight
+   */
+  async updateRange(text: string, version: string) {
+    try {
+      const result = await Promise.all(this.strategies.map((fn) => fn(text)));
+
+      const actualVersion = this.document.version.toString();
+      if (actualVersion !== version) {
+        // todo update env
+        if (process.env.COLOR_HIGHLIGHT_DEBUG)
+          throw new Error("Document version already has changed");
+
         return;
       }
-  
-      const text = this.document.getText();
-      const version = this.document.version.toString();
 
-      console.log(`DocumentHighlight.onUpdate: ${document.fileName} v${version}`);
-  
-      return this.updateRange(text, version);
-    }
-  
-    /**
-     * @param {string} text
-     * @param {string} version
-     *
-     * @memberOf DocumentHighlight
-     */
-    async updateRange(text : string, version : string) {
-      try {
-        const result = await Promise.all(this.strategies.map(fn => fn(text)));
-  
-        const actualVersion = this.document.version.toString();
-        if (actualVersion !== version) {
-            // todo update env
-          if (process.env.COLOR_HIGHLIGHT_DEBUG) throw new Error('Document version already has changed');
-  
-          return;
-        }
-  
-        const colorRanges = groupByColor(concatAll(result));
-  
-        if (this.disposed) {
-          return false;
-        }
-  
-        const updateStack = this.decorations.keys()
-          .reduce((state : Map<string, Range[]>, color : string) => {
-            state.set(color, []);
-            return state;
-          }, new Map());
-  
-        for (let [color, ranges] of colorRanges) {
-          updateStack.set(color, ranges.map((item : HighlightedRange) => {
+      const colorRanges = groupByColor(concatAll(result));
+
+      if (this.disposed) {
+        return false;
+      }
+
+      const updateStack = this.decorations
+        .keys()
+        .reduce((state: Map<string, Range[]>, color: string) => {
+          state.set(color, []);
+          return state;
+        }, new Map());
+
+      for (let [color, ranges] of colorRanges) {
+        updateStack.set(
+          color,
+          ranges.map((item: HighlightedRange) => {
             return new Range(
               this.document.positionAt(item.start),
               this.document.positionAt(item.end)
             );
-          }));
-        }
-  
-        for (let [color, updates] of updateStack) {
-          const decoration = this.decorations.get(color);
-  
-          window.visibleTextEditors
-            .filter(({ document }) => document.uri === this.document.uri)
-            .forEach(editor => editor.setDecorations(decoration, updates));
-        }
-      } catch (error) {
-        console.error(error);
+          })
+        );
       }
+
+      for (let [color, updates] of updateStack) {
+        const decoration = this.decorations.get(color);
+
+        window.visibleTextEditors
+          .filter(({ document }) => document.uri === this.document.uri)
+          .forEach((editor) => editor.setDecorations(decoration, updates));
+      }
+    } catch (error) {
+      console.error(error);
     }
-  
-    dispose() {
-      this.disposed = true;
-      this.decorations.dispose();
-      this.listener.dispose();
-  
+  }
+
+  dispose() {
+    this.disposed = true;
+    this.decorations.dispose();
+    this.listener.dispose();
+
     //   this.decorations = null;
     //   this.document = null;
     //   this.colors = null;
     //   this.listener = null;
-    }
   }
-  
-  function groupByColor(results : HighlightedRange[]) : Map<string, HighlightedRange[]> {
-    return results
-      .reduce((collection : Map<string, HighlightedRange[]>, item : HighlightedRange) => {
-        let list = collection.get(item.color);
-        if (!list) {
-            list = [];
-            collection.set(item.color, list);
-        }
-  
-        list.push(item);
-  
-        return collection;
-      }, new Map());
-  }
-  
-  function concatAll(arr : any[]) {
-    return arr.reduce((result, item) => result.concat(item), []);
-  }
-  
+}
+
+function groupByColor(
+  results: HighlightedRange[]
+): Map<string, HighlightedRange[]> {
+  return results.reduce(
+    (collection: Map<string, HighlightedRange[]>, item: HighlightedRange) => {
+      let list = collection.get(item.color);
+      if (!list) {
+        list = [];
+        collection.set(item.color, list);
+      }
+
+      list.push(item);
+
+      return collection;
+    },
+    new Map()
+  );
+}
+
+function concatAll(arr: any[]) {
+  return arr.reduce((result, item) => result.concat(item), []);
+}
--- src/strategies/dotnet-guid.ts
+++ src/strategies/dotnet-guid.ts
@@ -1,14 +1,30 @@
 import { HighlightedRange } from "./strategy-types";
+import { knownColors } from "../colors";
 
-export async function dotnetGuids(text : string) : Promise<HighlightedRange[]> {
-    const matches = text.match(/[\dA-F]{8}-[\dA-F]{4}-[\dA-F]{4}-[\dA-F]{4}-[\dA-F]{12}/gi);
+function guidToColor(guid: string): string {
+  guid = guid.replace(/-/g, "");
+  let groups: string[] = [];
+  for (let i = 0; i < guid.length; i += 8) {
+    groups.push(guid.slice(i, i + 8));
+  }
+  const hash = groups.map((group) => parseInt(group, 16)).reduce((acc, val) => acc ^ val, 0);
+  const color = knownColors[Math.abs(hash) % knownColors.length];
+  return color;
+}
+
+export async function dotnetGuids(text: string): Promise<HighlightedRange[]> {
+  // d — enable regex indices
+  // g — global search
+  // i — ignore case
+  const guidRegex = /\b[\dA-F]{8}-[\dA-F]{4}-[\dA-F]{4}-[\dA-F]{4}-[\dA-F]{12}\b/dgi;
+  const ranges: HighlightedRange[] = [];
+  while (true) {
+    const matches = guidRegex.exec(text);
     if (matches) {
-        return matches.map(match => new HighlightedRange(
-            text.indexOf(match),
-            text.indexOf(match) + match.length,
-            // todo construct color
-            "#FFFF00"
-        ));
+      const indices = matches.indices![0];
+      ranges.push(new HighlightedRange(indices[0], indices[1], guidToColor(matches[0])));
+    } else {
+      return ranges;
     }
-    return [];
+  }
 }
